1983969d6f102910622bc123ca95c6b85eda1d92
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-08-10 20:22:37 +0100

    ready to start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. OK!
+          Edit <code>src/App.js</code> It is time to start working on this project! OK?
         </p>
         <a
           className="App-link"
